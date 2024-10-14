087986bdf714d9f9a5958ec58d3b92c11f0e2914
Bick Renan <renan.bick@acad.ufsm.br>
2024-10-14 11:50:10 -0300

    feat: adição dos modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CarModule } from './car/car.module';
+import { StoreModule } from './store/store.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [CarModule, StoreModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
